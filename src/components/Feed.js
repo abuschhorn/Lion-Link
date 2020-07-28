d216c90125944a12531501270442e200c38b9742
Ashley Buschhorn <abuschhorn@gmail.com>
2020-07-28 16:04:48 -0500

    Updated Feed Form

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,23 +6,35 @@ export default function Feed({profiles}) {
     {
       userName: "Troy", 
       message: "jgirnkjsklfgkglwkwkdjdksl",
+      createdAt: new Date()
     }
 
   ])
+
   const addStatus = (status) => {
-    const tempStatus = [...statuses, status]
+    const tempStatus = [status, ...statuses]
     setStatuses(tempStatus)
   }
+
+
 
   return (
     <div>
     <FeedForm addStatus = {addStatus}/>
-  <div className="media">
-  <div className="media-body">
-   <h5 className="mt-0">{profiles[0].fName}</h5>
-    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-  </div>
-</div>
+  <ul className="list-unstyled">
+    {statuses && statuses.map((status) =>
+    (<li className="media">
+        <div class="media-body">
+      <h5 class="mt-0 mb-1">{status.userName}</h5>
+      {status.createdAt.toDateString()} {status.message}
+    </div>
+
+    </li>
+
+    )
+    
+    )}
+  </ul>
 </div>
 
   )
--- src/components/FeedForm.js
+++ src/components/FeedForm.js
@@ -4,17 +4,21 @@ export default function FeedForm ({addStatus}) {
     const [userName, setUserName] = useState("")
     const [message, setMessage] = useState("")
 
+
     const handleSubmit = (event) => {
 
         event.preventDefault();
-        addStatus({userName, message})
+        addStatus({userName, createdAt: new Date(), message})
+        setUserName('')
         setMessage("")
     }
     return (
         <div>
-            <form>
-                <input placeholder = "hello"/>
+            <form onSubmit={handleSubmit}>
+                <input className="form=control" value={userName} onChange={(event) => setUserName(event.target.value)} placeholder = "hello"/>
+                <input className="form=control" value={message} onChange={(event) => setMessage(event.target.value)} placeholder = "hello"/>
                 
+                <button onClick={handleSubmit}>Update Status</button>
             </form>
         </div>
     )

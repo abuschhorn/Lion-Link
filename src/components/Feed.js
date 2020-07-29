5141883d17ba62a59a20e970d63e53823d5269c6
luluriffic <laurenlamparo@gmail.com>
2020-07-29 12:56:27 -0700

    Styled Feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,41 +1,37 @@
-import React, {useState} from "react";
-import FeedForm from "./FeedForm" 
+import React, { useState } from "react";
+import FeedForm from "./FeedForm";
 
-export default function Feed({profiles}) {
-  const[statuses, setStatuses] = useState([
+export default function Feed({ profiles }) {
+  const [statuses, setStatuses] = useState([
     {
-      userName: "Troy", 
+      userName: "Troy",
       message: "jgirnkjsklfgkglwkwkdjdksl",
-      createdAt: new Date()
-    }
-
-  ])
+      createdAt: new Date(),
+    },
+  ]);
 
   const addStatus = (status) => {
-    const tempStatus = [status, ...statuses]
-    setStatuses(tempStatus)
-  }
-
-
+    const tempStatus = [status, ...statuses];
+    setStatuses(tempStatus);
+  };
 
   return (
     <div>
-    <FeedForm addStatus = {addStatus}/>
-  <ul className="list-unstyled">
-    {statuses && statuses.map((status) =>
-    (<li className="media">
-        <div class="media-body">
-      <h5 class="mt-0 mb-1">{status.userName}</h5>
-      {status.createdAt.toDateString()} {status.message}
+      <FeedForm addStatus={addStatus} />
+      <ul className='list-unstyled'>
+        {statuses &&
+          statuses.map((status) => (
+            <li className='media'>
+              <div class='media-body card'>
+                <h5 class='mt-0 mb-1'>{status.userName}</h5>
+                <div className='status-date'>
+                  {status.createdAt.toDateString()}
+                </div>
+                {status.message}
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
-
-    </li>
-
-    )
-    
-    )}
-  </ul>
-</div>
-
-  )
+  );
 }

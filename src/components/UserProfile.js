ce9b049eb136b2e5f46f8c590876e03957808c6f
luluriffic <laurenlamparo@gmail.com>
2020-07-30 13:50:37 -0700

    Styled AboutPage, FeedForm, and UserProfile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,16 +1,23 @@
 import React from "react";
 import LionPurple from "../profileImages/lion-purple.png";
-import { Image } from "react-bootstrap";
+import { Image, Media } from "react-bootstrap";
 
 export default function UserProfile() {
   return (
     <div>
-      <h1> Name: </h1>
-      <Image
-        src='https://firebasestorage.googleapis.com/v0/b/lion-link-6800a.appspot.com/o/lion-blue.png?alt=media&token=5f6bd5f9-d8d8-4593-a70d-98c7e28ac971'
-        height='60'
-        width='60'
-      />
+      <h1 className='display-4'> John Doe </h1>
+      <Media className='align-self-center mr-3'>
+        <Image
+          src='https://firebasestorage.googleapis.com/v0/b/lion-link-6800a.appspot.com/o/lion-blue.png?alt=media&token=5f6bd5f9-d8d8-4593-a70d-98c7e28ac971'
+          height='60'
+          width='60'
+        />
+        <Media.Body>
+          Major: Math Minor: CS Interest: Sushi Religion: Catholic Sexuality:
+          Straight Politics: Conservative Astrology: Scorpio Diet: Omnivore
+          Instagram: @johnny.doe
+        </Media.Body>
+      </Media>
     </div>
   );
 }

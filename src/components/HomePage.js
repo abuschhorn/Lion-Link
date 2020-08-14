c8c301665956c50adc313d35a2c2d2c97eb84aa6
luluriffic <laurenlamparo@gmail.com>
2020-08-13 18:47:01 -0700

    Fixed Aboutpage Images and Created Homepage buttons

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,7 +9,11 @@ export default function HomePage() {
         style={{
           backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.6) 100%,rgba(255,255,255,0.9) 100%), url(${bigLogo})`,
         }}>
-        <div className='container homepage'></div>
+        <div className='container homepage'>
+          <button className='btn btn-danger'>LOGIN</button>
+          <span class='glyphicon glyphicon-edit' aria-hidden='true'></span>
+          <button className='btn btn-danger margin-left'>CREATE PROFILE</button>
+        </div>
       </div>
     </div>
   );
